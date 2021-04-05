7e2b063c982dfaf2b8da111ec76b2f0c2882fb0f
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-04-05 22:08:32 +0000

    Clean up DivaHitWindows + remove negative hitwindow, did not fix it

--- osu.Game.Rulesets.Diva/Scoring/DivaHitWindows.cs
+++ osu.Game.Rulesets.Diva/Scoring/DivaHitWindows.cs
@@ -4,28 +4,21 @@ namespace osu.Game.Rulesets.Diva.Scoring
 {
     public class DivaHitWindows : HitWindows
     {
-        private static readonly DifficultyRange[] diva_ranges =
+        protected override DifficultyRange[] GetRanges() => new DifficultyRange[]
         {
-            new DifficultyRange(HitResult.Perfect, 80, 50, -20),
-            new DifficultyRange(HitResult.Great, 140, 100, 60),
-            new DifficultyRange(HitResult.Meh, 200, 150, 100),
-            new DifficultyRange(HitResult.Miss, 300, 350, 400),
+            new(HitResult.Perfect, 80, 50, 20),
+            new(HitResult.Great, 140, 100, 60),
+            new(HitResult.Meh, 200, 150, 100),
+            new(HitResult.Miss, 300, 350, 400),
         };
 
-        public override bool IsHitResultAllowed(HitResult result)
-        {
-            switch (result)
-            {
-                case HitResult.Perfect:
-                case HitResult.Great:
-                case HitResult.Meh:
-                case HitResult.Miss:
-                    return true;
-            }
-
-            return false;
-        }
-
-        protected override DifficultyRange[] GetRanges() => diva_ranges;
+        public override bool IsHitResultAllowed(HitResult result) =>
+            result switch {
+                HitResult.Perfect => true,
+                HitResult.Great => true,
+                HitResult.Meh => true,
+                HitResult.Miss => true,
+                _ => false
+            };
     }
 }
